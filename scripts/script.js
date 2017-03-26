adc9bea47afe2051d004a12ea0bb2fcda6648595
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-03-26 19:16:11 -0300

    Display data from csv

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,49 @@
 const { dialog } = require('electron').remote;
+const csv = require('fast-csv');
 
-angular.module('mailApp', [])
-  .controller('MailController', ($scope) => {
-    $scope.title = 'Erik write this';
+const select = document.querySelector('#select');
+const error = document.querySelector('#error');
+const list = document.querySelector('#list');
 
-    $scope.openFile = () => {
-      dialog.showOpenDialog({
-        properties: ['openFile'],
-        filters: [
-          {name: 'All Files', extensions: ['csv']}
-        ]
-      }, (fileName) => console.log(fileName));
-    }
+select.addEventListener('click', () => {
+  dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      {name: 'All Files', extensions: ['csv']}
+    ]
+  }, (fileName) => {
+    if (fileName === undefined) return;
+
+    let users = [];
+
+    const content = csv.fromPath(fileName[0], { delimiter: ';' })
+      .on('data', data => users.push(data))
+      .on('end', () => {
+        delete users[0];
+        users = users.map((user) => {
+          return {
+            name: user[0],
+            email: user[1],
+            status: user[2]
+          };
+        });
+        displayUsers(users);
+      })
   });
+});
+
+function displayUsers (users) {
+  let display = users.forEach((user, key) => {
+    let tr = document.createElement('tr');
+    let tdName = document.createElement('td');
+    tdName.appendChild(document.createTextNode(user.name));
+    let tdEmail = document.createElement('td');
+    tdEmail.appendChild(document.createTextNode(user.email));
+    let tdStatus = document.createElement('td');
+    tdStatus.appendChild(document.createTextNode(user.status));
+    tr.appendChild(tdName);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdStatus);
+    list.appendChild(tr);
+  });
+}
